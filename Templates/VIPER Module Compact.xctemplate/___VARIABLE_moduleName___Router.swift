627d5722876f8cb8f276b7fdfdbda89223c61b2e
Issarapong Poesua <dekablade01@gmail.com>
2020-02-28 14:23:03 +0700

    Add compact template

---
+++ Templates/VIPER Module Compact.xctemplate/___VARIABLE_moduleName___Router.swift
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+___VARIABLE_sourceryAnnotationForProtocols___
+protocol ___VARIABLE_moduleName___RouterInput: AnyObject {
+}
+
+final class ___VARIABLE_moduleName___Router {
+
+    weak var view: ___VARIABLE_moduleName___ViewInput?
+
+    private var viewController: UIViewController? {
+        return view as? UIViewController
+    }
+}
+
+// MARK: - ___VARIABLE_moduleName___RouterInput
+
+extension ___VARIABLE_moduleName___Router: ___VARIABLE_moduleName___RouterInput {
+}
---
+++ Templates/VIPER Module Compact.xctemplate/___VARIABLE_moduleName___ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+___VARIABLE_sourceryAnnotationForProtocols___
+protocol ___VARIABLE_moduleName___ViewInput: AnyObject {
+
+    func configure()
+}
+
+___VARIABLE_sourceryAnnotationForProtocols___
+protocol ___VARIABLE_moduleName___ViewOutput: AnyObject {
+
+    func viewDidLoad()
+}
+
+final class ___VARIABLE_moduleName___ViewController: UIViewController {
+
+    var output: ___VARIABLE_moduleName___ViewOutput?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        output?.viewDidLoad()
+    }
+}
+
+// MARK: - ___VARIABLE_moduleName___ViewInput
+
+extension ___VARIABLE_moduleName___ViewController: ___VARIABLE_moduleName___ViewInput {
+
+    func configure() { 
+    }
+}
